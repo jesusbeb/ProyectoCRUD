99439385c0e16c54d695ea0fca2fb90592657b37
jesusbeb <jbeltranb@outlook.com>
2025-06-03 16:52:49 -0600

    Creacion de Proyecto Java con Maven y Gestion de Dependencias

---
+++ src/main/java/org/example/platzi/main/Main.java
@@ -0,0 +1,20 @@
+package org.example.platzi.main;
+/* Creamos un nuevo proyecto con Java y Maven
+Agregamos la dependencia de mysql connector en el pom.xml, en lugar de agregar el
+archivo jar. hay dos formas de hacerlo:
+1 - Vamos al repositorio de Maven y tecleamos "MySQL Connector/J" para buscarlo y
+elegimos la version deseada. Buscamos el codigo de Maven para copiarlo y pegarlo en
+del pom, dentro de <dependencies> </dependencies>. Actualizamos el proyecto en el
+icono de la m para cargar la dependencia.
+2 - Tecleamos Alt + Insert, Add dependency... y buscamos "MySQL Connector/J", damos
+clic en Add y listo
+*/
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+
+    }
+}
